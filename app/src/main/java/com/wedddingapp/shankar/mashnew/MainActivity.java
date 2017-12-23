14c72bd339cbcfd06a9b05d9268538e3e6c01d27
shankarm08 <shankarm08@gmail.com>
2017-12-23 13:58:28 +0530

    updated

---
+++ app/src/main/java/com/wedddingapp/shankar/mashnew/MainActivity.java
@@ -0,0 +1,202 @@
+package com.wedddingapp.shankar.mashnew;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private ImageView imageView;
+
+    // Request code for camera
+    private final int CAMERA_REQUEST_CODE = 100;
+
+    // Request code for runtime permissions
+    private final int REQUEST_CODE_STORAGE_PERMS = 321;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        imageView = (ImageView) findViewById(R.id.image);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                    if (!hasPermissions()){
+                        // your app doesn't have permissions, ask for them.
+                        requestNecessaryPermissions();
+                    }
+                    else {
+                        // your app already have permissions allowed.
+                        // do what you want.
+                        dispatchTakePictureIntent();
+                    }
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Camera not supported", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private boolean hasPermissions() {
+        int res = 0;
+        // list all permissions which you want to check are granted or not.
+        String[] permissions = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        for (String perms : permissions){
+            res = checkCallingOrSelfPermission(perms);
+            if (!(res == PackageManager.PERMISSION_GRANTED)){
+                // it return false because your app dosen't have permissions.
+                return false;
+            }
+
+        }
+        // it return true, your app has permissions.
+        return true;
+    }
+
+    private void requestNecessaryPermissions() {
+        // make array of permissions which you want to ask from user.
+        String[] permissions = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // have arry for permissions to requestPermissions method.
+            // and also send unique Request code.
+            requestPermissions(permissions, REQUEST_CODE_STORAGE_PERMS);
+        }
+    }
+
+    /* when user grant or deny permission then your app will check in
+      onRequestPermissionsReqult about user's response. */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grandResults) {
+        // this boolean will tell us that user granted permission or not.
+        boolean allowed = true;
+        switch (requestCode) {
+            case REQUEST_CODE_STORAGE_PERMS:
+                for (int res : grandResults) {
+                    // if user granted all required permissions then 'allowed' will return true.
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+                }
+                break;
+            default:
+                // if user denied then 'allowed' return false.
+                allowed = false;
+                break;
+        }
+        if (allowed) {
+            // if user granted permissions then do your work.
+            dispatchTakePictureIntent();
+        }
+        else {
+            // else give any custom waring message.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    Toast.makeText(MainActivity.this, "Camera Permissions denied", Toast.LENGTH_SHORT).show();
+                }
+                else if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    Toast.makeText(MainActivity.this, "Storage Permissions denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("lpl", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+    private void setPic() throws Exception {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+    }
+    public String mCurrentPhotoPath;
+
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String folderName = "test";
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File f = new File(Environment.getExternalStorageDirectory(), folderName);
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+        File storageDir = new File(Environment.getExternalStorageDirectory()+"/"+folderName);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            try {
+                setPic();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
